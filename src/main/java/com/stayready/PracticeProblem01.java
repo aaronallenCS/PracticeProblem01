a3c87ffaf25d8933536dac398ebfbe87671a0e2b
beastlyCoder <imaaronacs@gmail.com>
2020-07-28 09:00:01 -0700

    Close to correct, missing some edge case logic, and the correct word spacing, tests included

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,30 @@
 package com.stayready;
 
-public class PracticeProblem01 {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PracticeProblem01
+{
+    public static List<String> palindromePartition(String s)
+    {
+        List<String> arrList = new ArrayList<>();
+        if(isPalindrome(s))
+        {
+            int low = 0;
+            int high = s.length() - 1;
+            arrList.add(s.substring(low, high + 1).replaceAll("", " "));
+            while(low <= s.length() && high >= low && s.charAt(low) == s.charAt(high))
+            {
+                arrList.add(s.substring(low, high + 1).replaceAll("", ""));
+                low++;
+                high--;
+            }
+        }
+        return arrList;
+    }
+
+    public static boolean isPalindrome(String s)
+    {
+        return new StringBuilder(s).reverse().toString().equals(s);
+    }
 }
--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,30 @@
 package com.stayready;
 
-public class PracticeProblem01Test {
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PracticeProblem01Test
+{
+    @Test
+    public void palindromePartition()
+    {
+        List<String> expectedList = new ArrayList<String>();
+        expectedList.add("r a c e c a r");
+        expectedList.add("r aceca r");
+        expectedList.add("r a cec a r");
+
+        Assert.assertEquals(expectedList, new PracticeProblem01().palindromePartition("racecar"));
+    }
+
+    @Test
+    public void checkIfPalindrome()
+    {
+        boolean expected = true;
+        String car = "racecar";
+
+        Assert.assertEquals(expected, new PracticeProblem01().isPalindrome(car));
+    }
 }
